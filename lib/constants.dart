cdeb35ce3ab9753aa25b64239c7ea1ea94371e9a
miraskaldykhan <miraskaldykhan@gmail.com>
2022-07-01 18:01:51 +0600

    aa

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants{
-  static const String weatherAppIid = '02461b0d8629094b5799a3ec48e6047c';
+  static const String weatherAppIid = ;
   static const String weatherBaseScheme = 'https://';
   static const String weatherBaseUrlDomain = 'api.openweathermap.org';
   static const String weatherImgBaseUrlDomain = 'openweathermap.org';
